759a3deab1858347e425bc620cbbb0e33631c8a5
kaishi1998 <kaishi.sakigake0221@docomo.ne.jp>
2024-03-11 23:27:33 +0900

    確認をお願いします。

--- samuraitravel-1/src/main/java/com/example/samuraitravel/controller/AuthController.java
+++ samuraitravel-1/src/main/java/com/example/samuraitravel/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.example.samuraitravel.controller;
 
-import org.springframework.stereotype.Controller;
+ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+ @Controller
 public class AuthController {
-	@GetMapping("/login")
-	public String login() {
-		return "auth/login";
-	}
+     @GetMapping("/login")
+     public String login() {        
+         return "auth/login";
+     }
 }
